df7799b2b0158b1caf9b50d21b609e8cebef160c
willamis.santos <willamis.santos@sf300138.sefaz.pe.gov.br>
2018-11-29 15:49:38 -0300

    teste

--- src/main/java/com/willamissantos/cursomc/domain/Categoria.java
+++ src/main/java/com/willamissantos/cursomc/domain/Categoria.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+
 @Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
